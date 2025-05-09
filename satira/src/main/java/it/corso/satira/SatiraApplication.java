d7b951df94ff197516bfcc0ecc9dc5e96cf090cf
FlaminemFlam <fiammetta.degliesposti@gmail.com>
2025-05-09 10:02:18 +0200

    Initial commit

---
+++ satira/src/main/java/it/corso/satira/SatiraApplication.java
@@ -0,0 +1,13 @@
+package it.corso.satira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatiraApplication.class, args);
+	}
+
+}

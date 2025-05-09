b801a30888800b596d1112a509dc06bf5a7146be
Gianmarco Minisi <gia.minisi@gmail.com>
2025-05-09 15:09:35 +0200

    Update AdminService.java

--- satira/src/main/java/it/corso/satira/service/AdminService.java
+++ satira/src/main/java/it/corso/satira/service/AdminService.java
@@ -6,4 +6,8 @@ import jakarta.servlet.http.HttpSession;
 public interface AdminService {
     String controlloLogin(String username, String password, HttpSession session);
     Admin datiAdmin(Integer id);
+    void rimozionePost(Integer id, HttpSession session);
+    void modificaPost(Integer id, String operation, HttpSession session);
+    
+    String controlloUsername(String username);
 }

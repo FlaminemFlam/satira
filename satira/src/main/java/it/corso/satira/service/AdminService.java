c4165f4f3762db13c4809acab618883a86c3c99e
Gianmarco Minisi <gia.minisi@gmail.com>
2025-05-09 15:00:41 +0200

    Update AdminService.java

--- satira/src/main/java/it/corso/satira/service/AdminService.java
+++ satira/src/main/java/it/corso/satira/service/AdminService.java
@@ -1,8 +1,9 @@
 package it.corso.satira.service;
 
+import it.corso.satira.model.Admin;
 import jakarta.servlet.http.HttpSession;
 
 public interface AdminService {
     String controlloLogin(String username, String password, HttpSession session);
-    
+    Admin datiAdmin(Integer id);
 }

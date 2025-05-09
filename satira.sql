857ae590e03ff031f3395bfea4fe3c5f970d351b
ElenaSquillero <elena.squillero@gmail.com>
2025-05-09 11:26:19 +0200

    SQL

---
+++ satira.sql
@@ -0,0 +1,129 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Creato il: Mag 09, 2025 alle 11:22
+-- Versione del server: 10.4.28-MariaDB
+-- Versione PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `satira`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `nikname` varchar(50) NOT NULL,
+  `pass` varchar(50) NOT NULL,
+  `img` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `commenti`
+--
+
+CREATE TABLE `commenti` (
+  `id` int(11) NOT NULL,
+  `testo` varchar(255) DEFAULT NULL,
+  `immagine_commento` longtext DEFAULT NULL,
+  `data_commento` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `post`
+--
+
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL,
+  `immagine_post` longtext DEFAULT NULL,
+  `titolo` varchar(100) NOT NULL,
+  `contenuto` text DEFAULT NULL,
+  `data_pubblicazione` datetime NOT NULL,
+  `fk_id_admin` int(11) NOT NULL,
+  `fk_id_commenti` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `nikname` (`nikname`);
+
+--
+-- Indici per le tabelle `commenti`
+--
+ALTER TABLE `commenti`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_id_admin` (`fk_id_admin`),
+  ADD KEY `fk_id_commenti` (`fk_id_commenti`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `commenti`
+--
+ALTER TABLE `commenti`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `post`
+--
+ALTER TABLE `post`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`fk_id_admin`) REFERENCES `admin` (`id`),
+  ADD CONSTRAINT `post_ibfk_2` FOREIGN KEY (`fk_id_commenti`) REFERENCES `commenti` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

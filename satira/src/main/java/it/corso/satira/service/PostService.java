5ef1896703f6143cc7c0aed87b9ff0156b5283be
marcodldev <marcodelisi2@gmail.com>
2025-05-09 15:54:35 +0200

    + postservice, postserviceimpl

--- satira/src/main/java/it/corso/satira/service/PostService.java
+++ satira/src/main/java/it/corso/satira/service/PostService.java
@@ -1,11 +1,18 @@
 package it.corso.satira.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
 
 import it.corso.satira.model.Post;
 
 public interface PostService {
     List<Post> elencoPosts();
+
     Post datiPost(Integer id);
+
     String eliminazionePost(Integer id);
+
+    void registrazionePost(Post post, MultipartFile immaginePost, String titolo, String contenuto, LocalDateTime dataPubblicazione,Integer idCommento, Integer idAdmin);
 }
--- satira/src/main/java/it/corso/satira/service/PostServiceImpl.java
+++ satira/src/main/java/it/corso/satira/service/PostServiceImpl.java
@@ -1,10 +1,13 @@
 package it.corso.satira.service;
 
+import java.time.LocalDateTime;
+import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import it.corso.satira.model.Post;
 import it.corso.satira.repository.PostRepository;
@@ -14,6 +17,12 @@ public class PostServiceImpl implements PostService {
 
   @Autowired
   private PostRepository postRepository;
+
+  @Autowired
+  private CommentiService commentiService;
+
+  @Autowired
+  private AdminService adminService;
 
   @Override
   public List<Post> elencoPosts() {
@@ -33,6 +42,28 @@ public class PostServiceImpl implements PostService {
   public String eliminazionePost(Integer id) {
     postRepository.deleteById(id);
     return null;
+  }
+
+  @Override
+  public void registrazionePost(Post post, MultipartFile immaginePost, String titolo, String contenuto,
+      LocalDateTime dataPubblicazione, Integer idCommento, Integer idAdmin) {
+    post.setTitolo(titolo);
+    post.setContenuto(contenuto);
+    post.setDataPubblicazione(dataPubblicazione);
+    post.setCommento(commentiService.datiCommento(idCommento));
+    post.setAdmin(adminService.datiAdmin(idAdmin));
+
+    if (immaginePost != null && !immaginePost.isEmpty()) {
+      try {
+        String formato = immaginePost.getContentType();
+        String immaginePostCodificata = "data:" + formato + ";base64,"
+            + Base64.getEncoder().encodeToString(immaginePost.getBytes());
+        post.setImmaginePost(immaginePostCodificata);
+      } catch (Exception e) {
+        System.out.println(e.getMessage());
+      }
+    }
+    postRepository.save(post);
   }
 
 }

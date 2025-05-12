7788c25558210ea6bf331e270bcf5bd794eddfcf
Gianmarco Minisi <gia.minisi@gmail.com>
2025-05-12 17:44:15 +0200

    Altri Controller

--- satira/src/main/java/it/corso/satira/controller/AdmindashController.java
+++ satira/src/main/java/it/corso/satira/controller/AdmindashController.java
@@ -1,66 +1,33 @@
 package it.corso.satira.controller;
 
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import it.corso.satira.model.Admin;
-import it.corso.satira.model.BecomeAdmin;
 import it.corso.satira.repository.AdminRepository;
-import it.corso.satira.service.EmailService;
 
 //localhost:8080/admidash
 @Controller
 @RequestMapping("/admindash")
 public class AdmindashController {
-    @Autowired
-    private EmailService emailService;
+
     
     @Autowired
     private AdminRepository adminRepository;
     
-    
-    
-    String token = UUID.randomUUID().toString(); //Creo un token univoco per l'invio mail grazie alla classe UUID di java.util
     
     @GetMapping
     public String renderPage(Model model){
         model.addAttribute("admin", new Admin());
         return "admindash";
     }
-    
-    
-    // Mostra il form precompilato
-    @GetMapping("/adminForm")
-    public String showAdminRequestForm(Model model) {
-        BecomeAdmin form = new BecomeAdmin();
-        // Imposta dei valori di default (se necessario)
-        form.setUsername("");       
-        form.setEmail("");
-        model.addAttribute("BecomeAdmin", form);
-        return "/adminForm"; 
-    }
-    
-    // Gestisce l'invio del form
-    @PostMapping("/adminForm")
-    public String processAdminRequest(@ModelAttribute("BecomeAdmin") BecomeAdmin form) {
-        // Costruisci il contenuto dell'email, sostituendo i placeholder con i valori del form
-        String confirmationLink = "http://localhost:8080/admindash?token=" + token;
-        String emailBody = "Ciao " + form.getUsername() + ",\n\n" +
-        "Per confermare il tuo accesso come admin, clicca sul seguente link:\n" +
-        confirmationLink + "\n\n" +
-        "Cordiali Saluti,\nIl Team";
-        emailService.sendMail("gia.minisi@gmail.com", "Conferma Admin", emailBody);
-        
-        return "redirect:/admindash/adminForm?success";
-    }
+
     
     @PostMapping("/create-admin")
     public String createAdmin(@RequestParam String username, @RequestParam String nickname, @RequestParam String password) {
--- satira/src/main/java/it/corso/satira/controller/FormAdminController.java
+++ satira/src/main/java/it/corso/satira/controller/FormAdminController.java
@@ -1,5 +1,49 @@
 package it.corso.satira.controller;
 
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import it.corso.satira.model.BecomeAdmin;
+import it.corso.satira.service.EmailService;
+
+@Controller
+@RequestMapping("/adminForm")
 public class FormAdminController {
 
+    @Autowired
+    private EmailService emailService;
+
+     String token = UUID.randomUUID().toString(); //Creo un token univoco per l'invio mail grazie alla classe UUID di java.util
+
+        // Mostra il form precompilato
+    @GetMapping
+    public String showAdminRequestForm(Model model) {
+        BecomeAdmin form = new BecomeAdmin();
+        // Imposta dei valori di default (se necessario)
+        form.setUsername("");       
+        form.setEmail("");
+        model.addAttribute("BecomeAdmin", form);
+        return "/adminForm"; 
+    }
+    
+    // Gestisce l'invio del form
+    @PostMapping
+    public String processAdminRequest(@ModelAttribute("BecomeAdmin") BecomeAdmin form) {
+        // Costruisci il contenuto dell'email, sostituendo i placeholder con i valori del form
+        String confirmationLink = "http://localhost:8080/admindash?token=" + token;
+        String emailBody = "Salve mi chiamo " + form.getUsername() + ",\n\n" +
+        "Richiedo di avere accesso come admin, clicca sul seguente link:\n" +
+        confirmationLink + "\n\n" +
+        "Cordiali Saluti,\nIl Team";
+        emailService.sendMail("gia.minisi@gmail.com", "Conferma Admin", emailBody);
+        
+        return "redirect:/adminForm?success";
+    }
 }

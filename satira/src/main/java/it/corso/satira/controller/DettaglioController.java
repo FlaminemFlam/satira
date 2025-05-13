d0ef26219eee1b4a6e26183f452b8668c6c66165
marcodldev <marcodelisi2@gmail.com>
2025-05-13 11:54:06 +0200

    + dettaglio

--- satira/src/main/java/it/corso/satira/controller/DettaglioController.java
+++ satira/src/main/java/it/corso/satira/controller/DettaglioController.java
@@ -1,6 +1,5 @@
 package it.corso.satira.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,8 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import it.corso.satira.model.Admin;
+import it.corso.satira.model.Commento;
 import it.corso.satira.model.Post;
+import it.corso.satira.service.AdminService;
+import it.corso.satira.service.CommentiService;
 import it.corso.satira.service.PostService;
+import it.corso.satira.service.CommentiService;
+import it.corso.satira.service.AdminService;
 
 @Controller
 @RequestMapping("/dettaglio")
@@ -18,13 +23,23 @@ public class DettaglioController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private CommentiService commentiService;
+
+    @Autowired
+    private AdminService adminService;
+
     @GetMapping
-    public String renderPage(@RequestParam(defaultValue = "0") Integer id, Model model){
+    public String renderPage(@RequestParam(defaultValue = "0") Integer id, Model model) {
         Post post = postService.datiPost(id);
-        if(post == null){
+        Commento commento = commentiService.datiCommento(id);
+        Admin admin = id == null ? new Admin() : adminService.datiAdmin(id);
+        if (post == null) {
             return "redirect:/";
         }
-        model.addAttribute("post",post);
+        model.addAttribute("post", post);
+        model.addAttribute("commento", commento);
+        model.addAttribute("admin", admin);
         return "dettaglio";
     }
 }

0c575c04075fa821ba15653d9e134b53fe8f5082
Gianmarco Minisi <gia.minisi@gmail.com>
2025-05-15 20:05:21 +0200

    Aggiunte le Gif

--- satira/src/main/java/it/corso/satira/controller/CommentoController.java
+++ satira/src/main/java/it/corso/satira/controller/CommentoController.java
@@ -1,14 +1,14 @@
 package it.corso.satira.controller;
 
-import java.io.IOException;
+
 import java.time.LocalDateTime;
-import java.util.Base64;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
+
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import it.corso.satira.model.Commento;
@@ -29,7 +29,7 @@ public class CommentoController {
     public String aggiungiCommento(
             @RequestParam("testo") String testo,
             @RequestParam("postId") Integer postId,
-            @RequestParam(value = "immagine", required = false) MultipartFile immagine,
+            @RequestParam(value = "immagine", required = false) String immagine,
             RedirectAttributes redirectAttributes) {
 
         // Crea un nuovo commento
@@ -39,22 +39,14 @@ public class CommentoController {
 
         // Gestione dell'immagine se presente
         if (immagine != null && !immagine.isEmpty()) {
-            try {
-                // Converte l'immagine in una stringa Base64
-                String base64Image = Base64.getEncoder().encodeToString(immagine.getBytes());
-                String imageFormat = immagine.getContentType();
-                String dataUri = "data:" + imageFormat + ";base64," + base64Image;
+            
+        // Salva l'immagine nel campo immagineCommento
+        commento.setImmagineCommento(immagine);
 
-                // Salva l'immagine nel campo immagineCommento
-                commento.setImmagineCommento(dataUri);
-            } catch (IOException e) {
-                e.printStackTrace();
-                // Gestire l'errore in qualche modo, ad esempio con un messaggio flash
-                redirectAttributes.addFlashAttribute("errorMessage", "Errore nel caricamento dell'immagine");
-                return "redirect:/dettaglio?id=" + postId;
-            }
-        }
+                
 
+
+    }
         // Imposta il post a cui si riferisce il commento
         Post post = postService.datiPost(postId);
         commento.setPost(post);
@@ -64,5 +56,5 @@ public class CommentoController {
 
         // Reindirizza alla pagina di dettaglio
         return "redirect:/dettaglio?id=" + postId;
-    }
+}
 }
